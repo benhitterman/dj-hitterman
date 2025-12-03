4107889f354fb746d9b4fccb7d1cfd14942a4830
benhitterman <benhitterman7@gmail.com>
2025-12-02 21:48:24 -0500

    added logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6b31673283687850"; }
+/******/ 		__webpack_require__.h = function() { return "fd95eed67c6831b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8f3c09eb1d0c3ab1"; }
+/******/ 	__webpack_require__.h = function() { return "fd95eed67c6831b1"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
@@ -82,6 +82,11 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 		};
 /******/ 	};
 /******/ }
+/******/ 
+/******/ /* webpack/runtime/nonce */
+/******/ !function() {
+/******/ 	__webpack_require__.nc = undefined;
+/******/ }();
 /******/ 
 /******/ }
 );

5a20ac24bfe8aec849e1debfe78363608e0b4b0a
benhitterman <benhitterman7@gmail.com>
2025-12-02 20:36:10 -0500

    added global styling

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "bbdf3716ef4a864c"; }
+/******/ 	__webpack_require__.h = function() { return "8f3c09eb1d0c3ab1"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
@@ -82,11 +82,6 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 		};
 /******/ 	};
 /******/ }
-/******/ 
-/******/ /* webpack/runtime/nonce */
-/******/ !function() {
-/******/ 	__webpack_require__.nc = undefined;
-/******/ }();
 /******/ 
 /******/ }
 );

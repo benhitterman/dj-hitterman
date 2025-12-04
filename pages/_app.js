a912e1070125077d8747a94f1a19d9e08dd81730
benhitterman <benhitterman7@gmail.com>
2025-12-03 20:35:51 -0500

    GA fixes v3

--- pages/_app.js
+++ pages/_app.js
@@ -6,24 +6,30 @@ import Script from 'next/script';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-FCNMEHS748"
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FCNMEHS748', { send_page_view: true });
+        `}
+      </Script>
+
+      {/* Head for favicon and title */}
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <title>DJ Hitterman</title>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-FCNMEHS748`}
-          strategy="afterInteractive"
-        />
-
-        <Script id="ga-init" strategy="afterInteractive">
-          {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-FCNMEHS748');
-`}
-        </Script>
       </Head>
+
+      {/* App component */}
       <Component {...pageProps} />
+
+      {/* Vercel Analytics */}
       <Analytics />
     </>
   );
